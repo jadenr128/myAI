6ec4c706187c3b63831f2361364917efec05ddb7
Jaden Rice <jrice1@unc.edu>
2025-03-04 16:28:41 -0500

    Updated all the customizations

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Welcome to UnderwriterX - Your Insurance Assistant`;
-export const MESSAGE_PLACEHOLDER: string = `Ask UnderwriterX anything about National or Illinois Insurance`;
+export const CHAT_HEADER: string = `Welcome to InsureBot 101 - Your Insurance Assistant`;
+export const MESSAGE_PLACEHOLDER: string = `Ask InsureBot 101 anything about your current insurance needs`;
 export const FOOTER_MESSAGE: string = `Customized AI by Jaden Rice @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
-export const PAGE_TITLE: string = `UnderwriterX - AI Insurance Assistant`;
+export const PAGE_TITLE: string = `InsureBot 101 - AI Insurance Assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
