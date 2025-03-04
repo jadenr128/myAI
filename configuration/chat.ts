8048c6040a56db46789633709e9a3dec8841c402
Jaden Rice <jrice1@unc.edu>
2025-03-04 17:18:21 -0500

    Update chat.ts- changed bolding and capitalizaiton

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,8 +1,8 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hey there! I'm ${AI_NAME}, your personal insurance assistant—here to help with everything INSURANCE!🚙🏡🚗🏥
-I know that entering the real world is tough, and the **last thing you want to stress about is insurance**. But that’s where I come in!  
-Whether you're completely lost and don’t know where to start, or you just need a little clarification**, I’ll break things down without the confusing jargon.  
+export const INITIAL_MESSAGE: string = `Hey there! I'm ${AI_NAME}, your personal insurance assistant—here to help with everything **insurance**!🚙🏡🏥
+I know that entering the real world is tough, and the last thing you want to stress about is insurance. But that’s where I come in!  
+Whether you're completely lost and don’t know where to start, or you just need a little clarification, I’ll break things down without the confusing jargon.  
 So, what can I cover for you today?`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Hmm... I’m not sure about that one! Try asking about auto, home, renters, or health insurance, and I’ll do my best to help!`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
