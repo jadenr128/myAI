dcee2ed4c2b0149f78bdbfb7ab13686def90a19a
Jaden Rice <jrice1@unc.edu>
2025-03-04 14:28:15 -0500

    Changed clear button to clear chat

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,7 +3,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `Welcome to UnderwriterX - Your Insurance Assistant`;
 export const MESSAGE_PLACEHOLDER: string = `Ask UnderwriterX anything about National or Illinois Insurance`;
 export const FOOTER_MESSAGE: string = `Customized AI by Jaden Rice @ UNC`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
